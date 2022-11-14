b7be03a761fce9cac9bf98f7cc131a5d8ab1e822
Oseias Costa <oseiascosta@iMac-de-Oseias.local>
2022-11-13 22:37:51 -0300

    Update for deploy

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use((req, res, next) => {
 
 app.use(express.json())
 
-app.use('/.netlify/functions', workoutRoutes)
+// app.use('/.netlify/functions', workoutRoutes)
 // routes
-// app.use('/api/workouts', workoutRoutes)
+app.use('/api/workouts', workoutRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
